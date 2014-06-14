bca9d086bfa95b9359842b99a7a2838f150cf274
OpenShift guest <syumimel@gmail.com>
2014-06-14 16:20:49 -0400

    added phpmenu.php

---
+++ hello-world.php
@@ -0,0 +1,16 @@
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+printf('<hr>');
+printf('GoodMorning2');
+printf('<hr>');
+    phpinfo();
+
+
+
+
+
+echo 'Hello world from Cloud9!';
+
+?>
