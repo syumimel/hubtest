d0d6411bc88eeddeea9e6e90c654772295ded01e
OpenShift guest <syumimel@gmail.com>
2014-06-14 22:39:20 -0400

    test2

--- test.php
+++ test.php
@@ -3,7 +3,26 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 printf('<hr>');
+$a=1;
+$b=1;
+
+if($a==1){
+ if($b==1){
+  printf("ok");  
+ }    
+}else{
+
+
+
+}
+
+    
+
 printf('GoodMorning42yorosikune');
+printf('<hr>');
+printf('test');
+printf('<hr>');
+printf('test');
 printf('<hr>');
     phpinfo();
 echo 'Hello world from Cloud9!';
